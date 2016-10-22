5b33ae111af7b0271df008a3b2d3c473af9651a3
Jorge Yau <codenameyau@gmail.com>
2016-10-22 10:55:36 -0400

    Update osx provisioning script

--- provision/osx.sh
+++ provision/osx.sh
@@ -6,7 +6,7 @@ mkdir -p $HOME/Workspace
 # Install Homebrew.
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install various brew dev packages.
+# Brew dev packages.
 brew update
 brew install git
 brew install python
@@ -14,18 +14,24 @@ brew install python3
 brew install ruby
 brew install nvm
 
+# Brew GNU default commands.
+brew install coreutils
+brew install gnu-sed --with-default-names
+
+# Brew terminal plugins (requires .bash_profile).
+brew install source-highlight
+brew install bash-completion
+brew install ack
+
 # Install useful python third-party tools.
 pip3 install tldr
 pip3 install requests
 pip3 install virtualenv
 
-# Brew terminal plugins (requires .bash_profile).
-brew install wget
-brew install source-highlight
-brew install bash-completion
-
 # Download personal dotfiles from github.
-wget https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.bash_profile -P ~
+wget https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.ackrc -P ~
 wget https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.bash_alias -P ~
 wget https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.bash_extras -P ~
+wget https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.bash_profile -P ~
 wget https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.gitconfig -P ~
+wget https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.gitignore_global -P ~
