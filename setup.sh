6be2e9e37996978145885f8af56d6139c9cef93e
Jorge Yau <codenameyau@gmail.com>
2017-09-27 23:12:51 -0400

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# TODO: Fix this outputs are being inversely displayed.
+print_update() {
+  echo $1 $2
+  echo $(cmp "$1" "$2")
+  if [ $(cmp --silent $1 $2) ]; then
+    echo "[+] Copied file: $1 to $2"
+  else
+    echo "[-] Failed to copy file: $1 to $2"
+  fi
+}
+
+# Update git directory.
+chmod u+x git/*
+cp git/post-commit-dotfiles .git/hooks/post-commit
+
+# Copy and overwrite dotfiles.
+cp shell/.bash_alias ~/.bash_alias
+cp shell/.gitconfig ~/.gitconfig
+cp shell/.gitignore_global ~/.gitignore_global
+
+# Log outputs.
+print_update "shell/.bash_alias" "~/.bash_alias"
+print_update "shell/.gitconfig" "~/.gitconfig"
+print_update "shell/.gitignore_global" "~/.gitignore_global"
+
+# Differentiate between .bashrc (linux) and .bash_profile (mac)
+if [ -f ~/.bashrc ]; then
+  cp shell/.bashrc ~/.bashrc
+  print_update "shell/.bashrc" "~/.bashrc"
+fi
+
+if [ -f ~/.bash_profile ]; then
+  cp shell/.bash_profile ~/.bash_profile
+  print_update "shell/.bash_profile" "~/.bash_profile"
+fi
