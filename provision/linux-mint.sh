f39f795eda49972a79efe3ead358f4f78c00a6ea
Jorge Yau <codenameyau@gmail.com>
2017-11-05 13:32:54 -0500

    Add heroku to linux provisioning

--- provision/linux-mint.sh
+++ provision/linux-mint.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-# Description: Provisioning script for Linux Mint
+
+# Third party PPA
+sudo add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
+curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
+
+# Update packages
 sudo apt-get update
+
+# Description: Provisioning script for Linux Mint
 sudo apt-get install -y xsel
+
+# Install heroku CLI
+sudo apt-get install heroku
+
