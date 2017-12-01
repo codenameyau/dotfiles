d278917bcd0e8930cafb8492596502778116d69c
Jorge Yau <codenameyau@gmail.com>
2017-12-01 03:18:35 -0500

    Add terminator and node to provision

--- provision/linux-mint.sh
+++ provision/linux-mint.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
+# Core utils.
+sudo apt-get update
+sudo apt-get install -y build-essential
+
 # Add Personal Package Archives (ppa)
 sudo add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
 sudo add-apt-repository ppa:git-core/ppa
 sudo apt-get update
 
-# Install heroku.
+# Install heroku and signature key.
 curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
-sudo apt-get install heroku
+sudo apt-get install -y heroku
+
+# Install git.
+sudo apt-get install -y git
+
+# Install node.js
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 # Install other third party packages.
-sudo apt-get install -y xsel
+sudo apt-get install -y xsel terminator
+
+# Enable trackpad gestures.
 
 # Make custom directories.
 mkdir -p $HOME/Workspace
