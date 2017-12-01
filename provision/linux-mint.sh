2b7ecb55e8a7effa7558cf0e9f2a52a89c0d330c
Jorge Yau <codenameyau@gmail.com>
2017-12-01 03:01:17 -0500

    Update bashrc

--- provision/linux-mint.sh
+++ provision/linux-mint.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Add dotfiles.
-wget_file .bash_extras https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.bash_extras
-wget_file .bash_prompt https://raw.githubusercontent.com/codenameyau/shiny-prompt/master/src/.bash_prompt
-wget_file .bash_aliases https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.bash_aliases
-wget_file .bashrc https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.bashrc
-wget_file .gitconfig https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.gitconfig
-wget_file .gitignore_global https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.gitignore_global
-
 # Add Personal Package Archives (ppa)
 sudo add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
 sudo add-apt-repository ppa:git-core/ppa
@@ -22,3 +14,11 @@ sudo apt-get install -y xsel
 
 # Make custom directories.
 mkdir -p $HOME/Workspace
+
+# Add dotfiles.
+wget_file .bash_extras https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.bash_extras
+wget_file .bash_prompt https://raw.githubusercontent.com/codenameyau/shiny-prompt/master/src/.bash_prompt
+wget_file .bash_aliases https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.bash_aliases
+wget_file .bashrc https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.bashrc
+wget_file .gitconfig https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.gitconfig
+wget_file .gitignore_global https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.gitignore_global
