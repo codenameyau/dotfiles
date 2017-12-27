b1a3161e081d8dfd41c149aabb670dbccb801357
Jorge Yau <codenameyau@gmail.com>
2017-12-27 17:40:22 -0500

    Update linux provisioning

--- provision/linux-mint.sh
+++ provision/linux-mint.sh
@@ -1,20 +1,38 @@
 #!/bin/bash
 
+#####################################################################
+# Personal Setup
+#####################################################################
+
+mkdir -p $HOME/Workspace
+wget -P $HOME https://raw.githubusercontent.com/codenameyau/shiny-prompt/master/src/.bash_prompt
+wget -P $HOME https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.bash_extras
+wget -P $HOME https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.bash_aliases
+wget -P $HOME https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.bashrc
+wget -P $HOME https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.gitconfig
+wget -P $HOME https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.gitignore_global
+
+#####################################################################
+# Package Installation
+#####################################################################
+
 # Core utils.
-sudo apt-get update
 sudo apt-get install -y build-essential
 
-# Add Personal Package Archives (ppa)
-sudo add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
+# Install git and git-lfs (large file storage).
 sudo add-apt-repository ppa:git-core/ppa
-sudo apt-get update
+sudo apt-get update -y
+sudo apt-get install -y git
+sudo apt-get install -y software-properties-common
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | os=ubuntu dist=trusty sudo -E bash
+sudo apt-get install git-lfs
+git lfs install
 
-# Install heroku and signature key.
+# Install heroku.
+sudo add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
+sudo apt-get update -y
 curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
 sudo apt-get install -y heroku
-
-# Install git.
-sudo apt-get install -y git
 
 # Install other third party packages.
 sudo apt-get install -y xsel terminator
@@ -26,15 +44,4 @@ sudo apt-get install -y nodejs
 # Install npm packages.
 sudo npm install -g tldr http-server
 
-# Enable trackpad gestures.
-
-# Make custom directories.
-mkdir -p $HOME/Workspace
-
-# Add dotfiles.
-wget -P $HOME https://raw.githubusercontent.com/codenameyau/shiny-prompt/master/src/.bash_prompt
-wget -P $HOME https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.bash_extras
-wget -P $HOME https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.bash_aliases
-wget -P $HOME https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.bashrc
-wget -P $HOME https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.gitconfig
-wget -P $HOME https://raw.githubusercontent.com/codenameyau/dotfiles/master/shell/.gitignore_global
+# TODO: Enable trackpad gestures.
