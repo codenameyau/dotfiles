d712fa1e03af6e24be4901b59ff7ab4df45b60f6
Jorge Yau <codenameyau@gmail.com>
2019-10-20 23:13:26 -0400

    Update osx provision

--- provision/osx.sh
+++ provision/osx.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 # Make directories if not exist.
-mkdir -p $HOME/Workspace
+mkdir -p "$HOME/Workspace"
+
+#####################################################################
+# PACKAGES
+#####################################################################
 
 # Install Homebrew.
 if [ ! -x "$(command -v brew)" ]; then
@@ -35,10 +39,92 @@ sudo pip3 install requests virtualenv
 # Install common nodejs third-party tools.
 sudo npm install -g http-server eslint webpack jest tldr
 
+
+#####################################################################
+# OSX CONFIG
+# https://github.com/mathiasbynens/dotfiles/blob/master/.macos
+#####################################################################
+
+# Close any open System Preferences panes, to prevent overriding.
+osascript -e 'tell application "System Preferences" to quit'
+
+# Automatically quit printer app once the print jobs complete
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+# Disable machine sleep while charging
+# sudo pmset -c sleep 0
+
+# Never go into computer sleep mode
+# sudo systemsetup -setcomputersleep Off > /dev/null
+
+# Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Save screenshots to the desktop
+defaults write com.apple.screencapture location -string "${HOME}/Desktop"
+
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
+
+# Show icons for hard drives, servers, and removable media on the desktop
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder: show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
+
+# When performing a search, search the current folder by default
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Disable the warning when changing a file extension
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Show indicator lights for open applications in the Dock
+defaults write com.apple.dock show-process-indicators -bool true
+
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Don’t show Dashboard as a Space
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# Don’t automatically rearrange Spaces based on most recent use
+defaults write com.apple.dock mru-spaces -bool false
+
+# Remove the auto-hiding Dock delay
+defaults write com.apple.dock autohide-delay -float 0
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Make Dock icons of hidden applications translucent
+defaults write com.apple.dock showhidden -bool true
+
+# Don’t show recent applications in Dock
+defaults write com.apple.dock show-recents -bool false
+
+
+#####################################################################
+# CUSTOM CONFIG
+#####################################################################
+
 # Download files via wget to home directory.
 wget_file() {
   if [ ! -f "$HOME/$1" ]; then
-    wget $2 -P $HOME
+    wget "$2" -P "$HOME"
   fi
 }
 
