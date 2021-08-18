d92040a48f234ffb68dcc64962a83b6d0c9dc914
Jorge Yau <codenameyau@users.noreply.github.com>
2021-08-18 13:01:33 -0400

    Create opt.py

---
+++ bin/opt.py
@@ -0,0 +1,17 @@
+"""
+You can also use this online OTP timed generator.
+https://codepen.io/Sudeepto/full/JbLbxe
+
+import pyotp
+
+>>> pyotp.random_base32()
+'6FLOGFWEHC66YRPM'
+>>> pyotp.TOTP('6FLOGFWEHC66YRPM').now()
+'423907'
+"""
+
+import pyotp
+secret = pyotp.random_base32()
+otp = pyotp.TOTP('6FLOGFWEHC66YRPM').now()
+print(secret)
+print(otp)
